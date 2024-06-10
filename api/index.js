cd3315d81ce2515825d106f3d52d03f1ddc9e8a3
omkarkumarpathak <134271180+omkarkumarpathak@users.noreply.github.com>
2024-06-10 18:01:55 +0530

    Connect MongoDB Database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app=express();
 
-app.listen(3000,()=>{
-    console.log("app is listening");
+mongoose.connect(process.env.MONGO)
+    .then(()=>{
+        console.log("MongoDb connected Successfully")
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+
+app.listen(4000,()=>{
+    console.log("hello")
 })
