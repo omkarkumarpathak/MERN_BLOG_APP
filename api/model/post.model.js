c2872b25bc3f6d51238305a104075881620babe3
omkarkumarpathak <134271180+omkarkumarpathak@users.noreply.github.com>
2024-07-08 20:16:25 +0530

    Created getPost API route, this route is useful for many queries

--- api/model/post.model.js
+++ api/model/post.model.js
@@ -16,7 +16,7 @@ const postSchema=new mongoose.Schema({
     },
     image:{
         type:String,
-        default:''
+        default:'https://th.bing.com/th/id/OIP.IIMoYouKRYtMO8_YM36T2wHaEK?w=272&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7'
     },
     category:{
         type:String,
--- api/router/post.route.js
+++ api/router/post.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import {createPost} from '../controller/post.controller.js'
+import {getPosts,createPost} from '../controller/post.controller.js'
 
 const router=express.Router();
 
 router.post('/create-post',verifyToken,createPost);
+router.get('/getPosts',getPosts);
 
 export default router;
